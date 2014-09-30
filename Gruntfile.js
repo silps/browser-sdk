0731115abd6b411517f6522aa1f3e952850cb033
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2014-09-30 12:20:33 +0200

    Grunt implementation

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    curl:{
+      'vendor/pubnub.min.js': 'https://raw.githubusercontent.com/pubnub/javascript/master/web/pubnub.min.js',
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+
+      build: {
+        src: ["lib/relayr.js"],
+        dest: 'build/relayr.min.js'
+      }
+    },
+    concat:{
+      dist:{
+        src: ["build/relayr.min.js", "vendor/pubnub.min.js"],
+        dest: "build/<%= pkg.name %>.min.js"
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task. 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-curl');
+
+  // Default task(s).
+  grunt.registerTask('default', ['curl','uglify','concat']);
+
+};
