7c8bb15510186641e5e93dbb81742c02883a30cf
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2015-02-20 16:25:02 +0100

    MQTT channel system

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     curl:{
-      'vendor/pubnub.min.js': 'https://raw.githubusercontent.com/pubnub/javascript/master/web/pubnub.min.js',
+      'vendor/mqttws31.min.js': 'http://git.eclipse.org/c/paho/org.eclipse.paho.mqtt.javascript.git/plain/src/mqttws31.js',
     },
     uglify: {
       options: {
@@ -12,13 +12,13 @@ module.exports = function(grunt) {
       },
 
       build: {
-        src: ["lib/relayr.js"],
+        src: ["lib/relayr.js", "vendor/mqttws31.min.js"],
         dest: 'build/relayr.min.js'
       }
     },
     concat:{
       dist:{
-        src: ["build/relayr.min.js", "vendor/pubnub.min.js"],
+        src: ["build/relayr.min.js"],
         dest: "build/<%= pkg.name %>.min.js"
       }
     },
@@ -31,8 +31,8 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task. 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-curl');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint','curl','uglify','concat']);
